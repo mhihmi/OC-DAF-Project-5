c37ba7ce598d3fc1c540312d7b178c29d58bd7f0
Idhem <Idhem@users.noreply.github.com>
2018-12-05 16:01:36 +0100

    edit jsquote.js Etape 1

--- P5/Etape 1/jsquote.js
+++ P5/Etape 1/jsquote.js
@@ -1,19 +1,55 @@
 // Etape 1 : des citations aléatoires
 
 // Déclaration des variables, tableaux de citations 
-const quote1a = [" A ", " B ", " C ", " D ", " E "];
-const quote1b = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 "];
-const quote1c = [" a ", " b ", " c ", " d ", " e "];
 
+const tab = {
+    style1: [
+        [" A ", " B ", " C ", " D ", " E "],
+        [" 1 ", " 2 ", " 3 ", " 4 ", " 5 "],
+        [" a ", " b ", " c ", " d ", " e "]
+    ],
+    style2: [
+        [" Zozo ", " Toto ", " Bouya ", " Ka ", " Zen "],
+        [" Roxor ", " Awesome ", " use ", " Set ", " Blop "],
+        [" Chaud ", " Froid ", " Zinzin ", " Zigoto ", " Noob "]
+    ]
+}
 
-function generateRandomQuote(tab1, tab2, tab3) {
-    // Je tire aléatoirement un nombre entre 0 et (longueur du tableau - 1)
-    const rand1 = Math.floor(Math.random() * tab1.length);
-    const rand2 = Math.floor(Math.random() * tab2.length);
-    const rand3 = Math.floor(Math.random() * tab3.length);
-    // Je concatène les données issues des tableaux et je la retourne au code appelant
-    return tab1[rand1] + tab2[rand2] + tab3[rand3];
+function generateRandomQuote(style) {
+    const tabs = tab[style];
+    let citation = '';
+
+    /*tabs.forEach(tab => {
+                    const index = Math.floor(Math.random() * tab.length);
+                    const text = tab[index];
+                    citation = citation + text;
+                });*/
+    // peut s'écrire aussi :            
+    for (let tab of tabs) {
+        const index = Math.floor(Math.random() * tab.length);
+        const text = tab[index];
+        citation = citation + " " + text;
+    }
+
+    return citation;
+
 };
 
-let citation = generateRandomQuote(quote1a, quote1b, quote1c);
-console.log(citation);
+function generateMultipleCitations(number, style) {
+    for (var i = 0; i < number; i++) {
+        const citation = generateRandomQuote(style);
+        console.log('citation : ' + i + ' - ' + citation);
+    }
+}
+
+
+generateMultipleCitations(5, 'style1');
+
+
+
+
+// let citation = generateRandomQuote('style1');
+// let citation2 = generateRandomQuote('style2');
+
+// console.log(citation);
+// console.log(citation2);
